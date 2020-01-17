1da0d1739721bc066c61ae52a28cec32aebb4da8
Maxim <31589216+Mmaaxxiimm@users.noreply.github.com>
2020-01-17 16:37:51 +0100

    Third commit
    With keyframe and animate-on-scroll animations

--- main.js
+++ main.js
@@ -14,3 +14,61 @@ menuToggler.addEventListener('click', function() {
 });
 
 
+//Scroll reveal
+//https://github.com/scrollreveal/scrollreveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300,
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
